9eb12f408951fc9b6a0222b4b3ae07ae493aa664
Mehmet Ali EROL <mehmetalierol@windowslive.com>
2023-04-15 10:33:11 +0300

    initial commit

---
+++ DockerCompose/PostgresWithPgAdmin/TableScripts.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS public."CrudTest"
+(
+    "Id" uuid NOT NULL,
+    "NameSurname" character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    "Email" character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    "Title" character varying(50) COLLATE pg_catalog."default",
+    "Phone" character varying(20) COLLATE pg_catalog."default",
+    "CreateDate" timestamp without time zone NOT NULL DEFAULT now(),
+    "Creator" uuid,
+    "IsActive" boolean NOT NULL DEFAULT true,
+    CONSTRAINT "CrudTest_pkey" PRIMARY KEY ("Id")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."CrudTest"
+    OWNER to postgres;
+
+
+CREATE TABLE IF NOT EXISTS public."MailLog"
+(
+    "Id" uuid NOT NULL,
+    "UserId" uuid NOT NULL,
+    "CreateDate" timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT "MailLogTest_pkey" PRIMARY KEY ("Id")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."MailLog"
+    OWNER to postgres;
