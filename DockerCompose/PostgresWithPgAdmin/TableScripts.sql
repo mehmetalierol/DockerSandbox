babb016975ac20efbaf02c3b8caf156c88a71af2
Mehmet Ali EROL <mehmetalierol@windowslive.com>
2023-07-16 14:02:30 +0300

    rabbitmqcluster

--- DockerCompose/PostgresWithPgAdmin/TableScripts.sql
+++ DockerCompose/PostgresWithPgAdmin/TableScripts.sql
@@ -1,3 +1,13 @@
+CREATE DATABASE "Training"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 CREATE TABLE IF NOT EXISTS public."CrudTest"
 (
     "Id" uuid NOT NULL,
---
+++ DockerRunCommands/CouchbaseDockerCommand.txt
@@ -0,0 +1,1 @@
+docker run -d --name db -p 8091-8096:8091-8096 -p 11210-11211:11210-11211 couchbase
